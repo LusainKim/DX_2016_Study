c41bac90bd02ed7f72ddb5d8c131c4cb83c7365d
Lusain_Kim <sain9212@gmail.com>
2016-08-03 06:20:39 +0900

    Rotating Cube 구현

--- DXMain/DXMain.cpp
+++ DXMain/DXMain.cpp
@@ -12,9 +12,13 @@
 #include <DirectXCollision.h>
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
 #include <chrono>
+
+#include <d3dcompiler.h>
+#pragma comment(lib, "d3dcompiler")
 
 using namespace std;
 using namespace std::chrono;
@@ -43,6 +47,14 @@ private:
 	HINSTANCE					m_hInstance					{ NULL }	;
 	HWND						m_hWnd						{ NULL }	;
 	RECT						m_rcClient								;
+
+	// Rendering 추가 부분 : Depth-Stencil 을 하지 않으면 정상적인 큐브가 출력되지 않는다.
+	ID3D11Texture2D				*m_pd3dDepthStencilBuffer	{ nullptr }	;
+	ID3D11DepthStencilView		*m_pd3dDepthStencilView		{ nullptr }	;
+
+	D3D11_VIEWPORT				m_screenViewport			{ NULL }	;
+	
+
 public:
 
 	CDXFramework() = default;
@@ -53,6 +65,13 @@ public:
 		if (m_pdxgiSwapChain) m_pdxgiSwapChain->Release();
 		if (m_pd3dRenderTargetView) m_pd3dRenderTargetView->Release();
 		if (m_pd3dDeviceContext) m_pd3dDeviceContext->Release();
+
+		// Rendering 추가 부분 : Depth-Stencil 을 하지 않으면 정상적인 큐브가 출력되지 않는다.
+		if (m_pd3dDepthStencilBuffer) m_pd3dDepthStencilBuffer->Release();
+		if (m_pd3dDepthStencilView) m_pd3dDepthStencilView->Release();
+
+		// Rendering 관련된, 세이더 등의 COM 객체는 별도로 해제
+		ReleaseShaderResource();
 	}
 
 	void Initialize(HINSTANCE hInstance, HWND hWnd)
@@ -173,7 +192,7 @@ public:
 			, D3D_FEATURE_LEVEL_10_0
 		};
 		// 지원 가능한 모든 하드웨어 기능 수준 집합 수를 구합니다.
-		size_t szFeatureLevel = GetArraySize(d3dFeatureLevels);
+		auto szFeatureLevel = static_cast<UINT>(GetArraySize(d3dFeatureLevels));
 
 		// 최종적으로 사용될 장치의 기능 수준을 저장하기 위한 변수입니다.
 		D3D_FEATURE_LEVEL nd3dFeatureLevel = D3D_FEATURE_LEVEL_11_1;
@@ -243,23 +262,473 @@ public:
 
 	bool CreateRenderTargetView()
 	{
-		ID3D11Texture2D *pd3dBackBuffer{ nullptr };
-		
-		m_pdxgiSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pd3dBackBuffer);
-		m_pd3dDevice->CreateRenderTargetView(pd3dBackBuffer, NULL, &m_pd3dRenderTargetView);
-		pd3dBackBuffer->Release();
-		m_pd3dDeviceContext->OMSetRenderTargets(1, &m_pd3dRenderTargetView, nullptr);
 
-	//	DXGI_FRAME_STATISTICS p;
-	//	m_pdxgiSwapChain->GetFrameStatistics(&p);
+		// Result Handle 입니다. 장치가 성공적으로 생성도는지 검사합니다.
+		HRESULT hResult = S_OK; 
+		
+		// render target 을 생성하기 위한 back buffer 를 SwapChain 에게 요청합니다.
+		ID3D11Texture2D *pd3dBackBuffer{ nullptr };
+
+		if (FAILED(hResult = m_pdxgiSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID *)&pd3dBackBuffer))) return(false);
+
+		// 반환받은 버퍼를 사용하여 render target view 를 생성합니다.
+		if (FAILED(hResult = m_pd3dDevice->CreateRenderTargetView(pd3dBackBuffer, NULL, &m_pd3dRenderTargetView))) return(false);
+
+		// back buffer 를 반환합니다.
+		if (pd3dBackBuffer) pd3dBackBuffer->Release();
+
+
+
+		// Rendering 추가 부분 : Depth-Stencil 을 하지 않으면 정상적인 큐브가 출력되지 않는다.
+		{
+			// depth stencil "texture" 를 생성합니다.
+			D3D11_TEXTURE2D_DESC d3dDepthStencilBufferDesc;
+	
+			// 메모리는 0으로 초기화합니다.
+			ZeroMemory(&d3dDepthStencilBufferDesc, sizeof(D3D11_TEXTURE2D_DESC));
+
+
+
+			// Width : texture 의 너비입니다.
+			d3dDepthStencilBufferDesc.Width = m_rcClient.right;
+			// Height : texture 의 높이입니다.
+			d3dDepthStencilBufferDesc.Height = m_rcClient.bottom;
+			// MipLevels : texture 최대 MipMap Level 수. 
+			//				다중 샘플링 텍스처 : 1
+			//				최대 밉맵 레벨 : 0
+			d3dDepthStencilBufferDesc.MipLevels = 1;
+			// ArraySize :texture 배열의 texture 개수. (배열이 아니면 1)
+			d3dDepthStencilBufferDesc.ArraySize = 1;
+			// Format : texture 픽셀 형식
+			d3dDepthStencilBufferDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+
+		#ifdef _WITH_MSAA4_MULTISAMPLING
+			d3dDepthStencilBufferDesc.SampleDesc.Count = 4;
+			d3dDepthStencilBufferDesc.SampleDesc.Quality = m_n4xMSAAQualities - 1;
+		#else
+			// SampleDesc : 다중 샘플링의 품질을 지정
+			// CheckMultisampleQualityLevels 함수를 사용하여 다중 샘플링 가능 여부를 확인한 뒤에 값 변경
+			{
+
+				// Count : 픽셀 당 샘플 개수
+				//	1  : 다중 샘플링을 하지 않음
+				//	2~ : 해당 수만큼의 다중 샘플링
+				d3dDepthStencilBufferDesc.SampleDesc.Count = 1;
+				// Quality : 품질 레벨
+				// 0 : 다중 샘플링을 하지 않음
+				d3dDepthStencilBufferDesc.SampleDesc.Quality = 0;
+			}
+		#endif
+
+			// Usage : texture 를 읽고 쓰는 방법에 대한 정의
+			d3dDepthStencilBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+			// BindFlags : 파이프라인 단계 어디에 연결할 것인지 정의
+			d3dDepthStencilBufferDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+			// MiscFlags : 리소스에 대한 추가 선택 사항. 사용하지 않으면 0.
+			d3dDepthStencilBufferDesc.MiscFlags = 0;
+			// CPUAccessFlags : CPU가 버퍼를 사용할 수 있는 유형. 사용하지 않으면 0.
+			d3dDepthStencilBufferDesc.CPUAccessFlags = 0;
+
+			//	기본 깊이-스텐실 버퍼의 깊이 버퍼를 읽으려고 한다.
+			if (FAILED(hResult = m_pd3dDevice->CreateTexture2D(&d3dDepthStencilBufferDesc, NULL, &m_pd3dDepthStencilBuffer))) return(false);
+
+			// Create the depth stencil view
+			D3D11_DEPTH_STENCIL_VIEW_DESC d3dDepthStencilViewDesc;
+			ZeroMemory(&d3dDepthStencilViewDesc, sizeof(D3D11_DEPTH_STENCIL_VIEW_DESC));
+			d3dDepthStencilViewDesc.Format = d3dDepthStencilBufferDesc.Format;
+
+		#ifdef _WITH_MSAA4_MULTISAMPLING
+			d3dDepthStencilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2DMS;
+		#else
+			d3dDepthStencilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+		#endif
+			d3dDepthStencilViewDesc.Texture2D.MipSlice = 0;
+			if (FAILED(hResult = m_pd3dDevice->CreateDepthStencilView(m_pd3dDepthStencilBuffer, &d3dDepthStencilViewDesc, &m_pd3dDepthStencilView))) return(false);
+
+
+		}
+
+		//	DXGI_FRAME_STATISTICS p;
+		//	m_pdxgiSwapChain->GetFrameStatistics(&p);
+
+
+
+		// 전체 창을 대상으로 하기 위한 3D 렌더링 뷰포트를 설정합니다.
+		m_screenViewport = D3D11_VIEWPORT{
+			  static_cast<FLOAT>(0.0f)
+			, static_cast<FLOAT>(0.0f)
+			, static_cast<FLOAT>(m_rcClient.right)
+			, static_cast<FLOAT>(m_rcClient.bottom)
+			, static_cast<FLOAT>(0.0f)
+			, static_cast<FLOAT>(1.0f)
+		};
+
+		// RS에 뷰포트 연결
+		m_pd3dDeviceContext->RSSetViewports(1, &m_screenViewport);
+
+
+		// Rendering Resource 생성
+		CreateRenderingResource();
+
 		return true;
 	}
 
-	using FLOAT4Array = float[4];
+	std::chrono::system_clock::time_point m_current_time = chrono::system_clock::now();
+	std::chrono::duration<double> m_timeElapsed; // 시간이 얼마나 지났나
+	double m_fps = 0.f;
+
+	using FLOAT4 = float[4];
+
 	void FrameAdvance()
 	{
-		m_pd3dDeviceContext->ClearRenderTargetView(m_pd3dRenderTargetView, FLOAT4Array{0.0f, 0.5f, 0.8f, 1.0f});
+		// Get tick
+		m_timeElapsed = chrono::system_clock::now() - m_current_time;
+#if 1
+		if (m_timeElapsed.count() > 0.016f)
+#else
+		if (true)
+#endif
+		{
+			m_current_time = chrono::system_clock::now();
+
+			if (m_timeElapsed.count() > 0.0)
+				m_fps = 1.0 / m_timeElapsed.count();
+		}
+		// 최대 FPS 미만의 시간이 경과하면 진행 생략
+		else return;
+
+		// 지속적인 상수버퍼의 갱신을 확인하기 위한 Update 함수
+		Update(static_cast<float>(m_timeElapsed.count()));
+
+		// OM에 RenderTarget 재설정
+		m_pd3dDeviceContext->OMSetRenderTargets(1, &m_pd3dRenderTargetView, m_pd3dDepthStencilView);
+
+		m_pd3dDeviceContext->ClearRenderTargetView(m_pd3dRenderTargetView, FLOAT4{0.0f, 0.5f, 0.8f, 1.0f});
+		// Rendering 추가 부분 : Depth-Stencil 을 하지 않으면 정상적인 큐브가 출력되지 않는다.
+		m_pd3dDeviceContext->ClearDepthStencilView(m_pd3dDepthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+		Render();
+
 		m_pdxgiSwapChain->Present(0, 0);
+	}
+	
+
+
+	// Shader Code
+private:
+	
+	vector<byte> ReadCSOFile(LPCTSTR path)
+	{
+		ifstream inputShaderCSO(path, ios::in | ios::binary);
+
+		if (!inputShaderCSO.is_open())
+		{
+			MessageBox(m_hWnd, TEXT("Shader Load Fail!"), TEXT("프로그램 구동 실패"), MB_OK);
+			return vector<byte>();
+		}
+
+		// size check;
+		inputShaderCSO.seekg(0, fstream::end);
+		size_t szData = inputShaderCSO.tellg();
+		inputShaderCSO.seekg(0);
+
+		vector<byte> retval;
+		retval.resize(szData);
+		
+		inputShaderCSO.read(reinterpret_cast<char*>(&(retval[0])), szData);
+		inputShaderCSO.close();
+
+		return retval;
+	}
+
+	// MVP 매트릭스를 꼭짓점 셰이더로 보내는 데 사용되는 상수 버퍼입니다.
+	struct ModelViewProjectionConstantBuffer
+	{
+		DirectX::XMFLOAT4X4 model;
+		DirectX::XMFLOAT4X4 view;
+		DirectX::XMFLOAT4X4 projection;
+	};
+
+	// 꼭짓점별 데이터를 꼭짓점 셰이더로 보내는 데 사용됩니다.
+	struct VertexPositionColor
+	{
+		DirectX::XMFLOAT3 pos;
+		DirectX::XMFLOAT3 color;
+	};
+	
+	ID3D11Buffer						*m_vertexBuffer			;
+	ID3D11Buffer						*m_indexBuffer			;
+	ID3D11Buffer						*m_constantBuffer		;
+
+	ID3D11VertexShader					*m_vertexShader			;
+	ID3D11PixelShader					*m_pixelShader			;
+
+	ID3D11InputLayout					*m_inputLayout			;
+	ID3D11RasterizerState				*m_pd3dRasterizerState	;
+	ModelViewProjectionConstantBuffer	m_constantBufferData	;
+
+	UINT								m_indexCount			;
+
+	void ReleaseShaderResource()
+	{
+		if (m_vertexBuffer)			m_vertexBuffer->Release();
+		if (m_indexBuffer)			m_indexBuffer->Release();
+		if (m_constantBuffer)		m_constantBuffer->Release();
+		if (m_vertexShader)			m_vertexShader->Release();
+		if (m_pixelShader)			m_pixelShader->Release();
+		if (m_pd3dRasterizerState)	m_pd3dRasterizerState->Release();
+		if (m_inputLayout)			m_inputLayout->Release();
+	}
+
+public:
+
+	void CreateRenderingResource()
+	{
+		XMMATRIX perspectiveMatrix = XMMatrixPerspectiveFovRH(
+			  // FOV Y 값 : 클 수록 멀리까지 볼 수 있다.
+			  60.0f * XM_PI / 180.0f
+			  // 종횡비
+			, static_cast<float>(m_rcClient.right / float(m_rcClient.bottom))
+			  // 최소 거리
+			, 0.01f
+			  // 최대 거리
+			, 100.0f
+		);
+
+		// 단위 4x4 행렬
+		XMMATRIX orientationMatrix(
+			1.0f, 0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 1.0f, 0.0f,
+			0.0f, 0.0f, 0.0f, 1.0f
+			);
+
+		// projection 에 값 입력
+		XMStoreFloat4x4(
+			&m_constantBufferData.projection,
+			XMMatrixTranspose(perspectiveMatrix * orientationMatrix)
+		);
+
+		// 시선은 y축을 따라 업 벡터가 있는 상태로 점 (0,-0.1,0)를 보며 (0,0.7,1.5)에 있습니다.
+		static const XMVECTORF32 eye = { 0.0f, 0.7f, 1.5f, 0.0f };
+		static const XMVECTORF32 at = { 0.0f, -0.1f, 0.0f, 0.0f };
+		static const XMVECTORF32 up = { 0.0f, 1.0f, 0.0f, 0.0f };
+
+		// 각자 각 입력.
+		XMStoreFloat4x4(&m_constantBufferData.view, XMMatrixTranspose(XMMatrixLookAtRH(eye, at, up)));
+		XMStoreFloat4x4(&m_constantBufferData.model, XMMatrixTranspose(XMMatrixRotationY(0.f)));
+
+		CreateShader();
+		CreateMesh();
+
+	}
+
+	void CreateShader()
+	{
+		// 번역된 hlsl인 cso 로딩 후 VS와 IL 생성 : 추후 멀티스레드로 구현을 해야 할 부분
+		auto loadVS = ReadCSOFile(TEXT("VertexShader.cso"));
+		{
+			m_pd3dDevice->CreateVertexShader(
+				  &(loadVS[0])
+				, loadVS.size()
+				, nullptr
+				, &m_vertexShader
+			);
+	
+			static const D3D11_INPUT_ELEMENT_DESC vertexDesc[] =
+			{
+				{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+				{ "COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+			};
+			m_pd3dDevice->CreateInputLayout(
+				  vertexDesc
+				, static_cast<UINT>(GetArraySize(vertexDesc))
+				, &(loadVS[0])
+				, loadVS.size()
+				, &m_inputLayout
+			);
+		}
+	
+		// 번역된 hlsl인 cso 로딩 후 PS 생성 : 추후 멀티스레드로 구현을 해야 할 부분
+		auto loadPS = ReadCSOFile(TEXT("PixelShader.cso"));
+		{
+			m_pd3dDevice->CreatePixelShader(
+				  &(loadPS[0])
+				, loadPS.size()
+				, nullptr
+				, &m_pixelShader
+			);
+	
+		}
+		
+		// 상수 버퍼 생성. 이와 다른 방식으로 선언/구현할 경우 정상 출력이 되지 않음. 추가 공부가 필요
+		CD3D11_BUFFER_DESC constantBufferDesc(sizeof(ModelViewProjectionConstantBuffer), D3D11_BIND_CONSTANT_BUFFER);
+
+		m_pd3dDevice->CreateBuffer(
+			  &constantBufferDesc
+			, nullptr
+			, &m_constantBuffer
+		);
+
+		// Create RasterizerState
+		D3D11_RASTERIZER_DESC d3dRasterizerDesc;
+		ZeroMemory(&d3dRasterizerDesc, sizeof(D3D11_RASTERIZER_DESC));
+		d3dRasterizerDesc.CullMode = D3D11_CULL_NONE;
+		d3dRasterizerDesc.FillMode = D3D11_FILL_WIREFRAME;
+		m_pd3dDevice->CreateRasterizerState(&d3dRasterizerDesc, &m_pd3dRasterizerState);
+
+	}
+
+	void CreateMesh()
+	{
+		
+		// 메시 꼭짓점을 로드합니다. 각 꼭짓점은 위치 및 색을 가지고 있습니다.
+		static const VertexPositionColor cubeVertices[] = 
+		{
+			{XMFLOAT3(-0.5f, -0.5f, -0.5f), XMFLOAT3(0.0f, 0.0f, 0.0f)},
+			{XMFLOAT3(-0.5f, -0.5f, +0.5f), XMFLOAT3(0.0f, 0.0f, 1.0f)},
+			{XMFLOAT3(-0.5f,  0.5f, -0.5f), XMFLOAT3(0.0f, 1.0f, 0.0f)},
+			{XMFLOAT3(-0.5f,  0.5f, +0.5f), XMFLOAT3(0.0f, 1.0f, 1.0f)},
+			{XMFLOAT3( 0.5f, -0.5f, -0.5f), XMFLOAT3(1.0f, 0.0f, 0.0f)},
+			{XMFLOAT3( 0.5f, -0.5f, +0.5f), XMFLOAT3(1.0f, 0.0f, 1.0f)},
+			{XMFLOAT3( 0.5f,  0.5f, -0.5f), XMFLOAT3(1.0f, 1.0f, 0.0f)},
+			{XMFLOAT3( 0.5f,  0.5f, +0.5f), XMFLOAT3(1.0f, 1.0f, 1.0f)},
+		};
+
+		D3D11_SUBRESOURCE_DATA vertexBufferData = { 0 };
+		vertexBufferData.pSysMem = cubeVertices;	// VertexPositionColor * GetArraySize(cubeVertices)
+		vertexBufferData.SysMemPitch = 0;
+		vertexBufferData.SysMemSlicePitch = 0;
+		CD3D11_BUFFER_DESC vertexBufferDesc(sizeof(cubeVertices), D3D11_BIND_VERTEX_BUFFER);
+
+		m_pd3dDevice->CreateBuffer(
+			  &vertexBufferDesc
+			, &vertexBufferData
+			, &m_vertexBuffer
+		);
+
+		// 메시 인덱스를 로드합니다. 각 인덱스의 3개 숫자는
+		// 화면에 렌더링할 삼각형을 나타냅니다.
+		// 예를 들어 0,2,1은 인덱스로 꼭짓점을 의미합니다.
+		// 0, 2 및 1인 꼭짓점이 이 메시의 첫 번째 삼각형을
+		// 구성함을 의미합니다.
+		static const unsigned short cubeIndices [] =
+		{
+			0,2,1, // -x
+			1,2,3,
+
+			4,5,6, // +x
+			5,7,6,
+
+			0,1,5, // -y
+			0,5,4,
+
+			2,6,7, // +y
+			2,7,3,
+
+			0,4,6, // -z
+			0,6,2,
+
+			1,3,7, // +z
+			1,7,5,
+		};
+
+		// 메시 인덱스 개수
+		m_indexCount = static_cast<UINT>(GetArraySize(cubeIndices));
+		// 메시 인덱스를 위한 인덱스 버퍼 생성
+		D3D11_SUBRESOURCE_DATA indexBufferData = { 0 };
+		indexBufferData.pSysMem = cubeIndices;
+		indexBufferData.SysMemPitch = 0;
+		indexBufferData.SysMemSlicePitch = 0;
+		CD3D11_BUFFER_DESC indexBufferDesc(sizeof(cubeIndices), D3D11_BIND_INDEX_BUFFER);
+
+		m_pd3dDevice->CreateBuffer(
+			  &indexBufferDesc
+			, &indexBufferData
+			, &m_indexBuffer
+		);
+
+	}
+
+	void Render()
+	{
+		// 그래픽 장치로 보낼 수 있도록 상수 버퍼를 준비합니다.
+		m_pd3dDeviceContext->UpdateSubresource(   m_constantBuffer
+												, 0
+												, NULL
+												, &m_constantBufferData
+												, 0
+												, 0
+		);
+
+		// 각 꼭짓점은 VertexPositionColor 구조체의 한 인스턴스입니다.
+		UINT stride = sizeof(VertexPositionColor);
+		UINT offset = 0;
+
+		// 꼭지점 버퍼를 연결합니다.
+		m_pd3dDeviceContext->IASetVertexBuffers(	  0
+													, 1
+													, &m_vertexBuffer
+													, &stride
+													, &offset
+		);
+
+		// 인덱스 버퍼를 연결합니다.
+		m_pd3dDeviceContext->IASetIndexBuffer(	  m_indexBuffer
+												, DXGI_FORMAT_R16_UINT // 각 인덱스는 하나의 부호 없는 16비트 정수(Short)입니다.
+												, 0
+		);
+
+		m_pd3dDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+		// Input Layout을 연결합니다.
+		m_pd3dDeviceContext->IASetInputLayout(m_inputLayout);
+
+		// 꼭짓점 셰이더를 연결합니다.
+		m_pd3dDeviceContext->VSSetShader(	  m_vertexShader
+											, nullptr
+											, 0
+		);
+
+		// 상수 버퍼를 그래픽 장치에 보냅니다.
+		m_pd3dDeviceContext->VSSetConstantBuffers(	  0
+													, 1
+													, &m_constantBuffer
+		);
+
+		// 레스터라이저 상태를 연결합니다.
+		m_pd3dDeviceContext->RSSetState(m_pd3dRasterizerState);
+
+		// 픽셀 셰이더를 연결합니다.
+		m_pd3dDeviceContext->PSSetShader(	  m_pixelShader
+											, nullptr
+											, 0
+		);
+
+		// 개체를 그립니다.
+		m_pd3dDeviceContext->DrawIndexed(	  m_indexCount
+											, 0
+											, 0
+		);
+
+	}
+
+
+
+	void Update(float fTimeElapsed)
+	{
+		static float fTotalTime = 0.f;
+		fTotalTime += fTimeElapsed;
+		float radiansPerSecond = XMConvertToRadians(45);
+		double totalRotation = fTotalTime * radiansPerSecond;
+		float radians = static_cast<float>(fmod(totalRotation, XM_2PI));
+		Rotate(radians);
+	}
+
+	void Rotate(float radians)
+	{
+		// 업데이트된 모델 매트릭스를 셰이더에 전달하도록 준비합니다.
+		XMStoreFloat4x4(&m_constantBufferData.model, XMMatrixTranspose(XMMatrixRotationY(radians)));
 	}
 
 };
@@ -318,7 +787,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 				DispatchMessage(&msg);
 			}
 		}
-		else fw.FrameAdvance();
+		else
+		{
+			fw.FrameAdvance();
+		}
 
     }
 
@@ -417,7 +889,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     case WM_PAINT:
         {
             PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
+		//	HDC hdc = 
+				BeginPaint(hWnd, &ps);
             // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다.
             EndPaint(hWnd, &ps);
         }
